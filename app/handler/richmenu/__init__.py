7c6ea12aa18494bc281538e8eee15cdbbceeb3de
SiriusKoan <me@siriuskoan.one>
2022-12-13 19:35:53 +0800

    fix style by black

--- app/handler/richmenu/__init__.py
+++ app/handler/richmenu/__init__.py
@@ -13,20 +13,21 @@ from modules.funtions import introT, toolsT
 from modules.map import locationT
 from modules.recruitment import recruitmentT, recruitmentUtil
 
+
 class RichmenuHandler:
     def __init__(self, line_bot_api, user_instance):
         self.line_bot_api = line_bot_api
         self.user = user_instance
-    
-    def detect(self, msg):
-        '''判斷是否為選單事件
-            Params:
-                - msg
-            Return:
-                - isRichmenu(bool): 若開頭為 [選單]
-        '''
 
-        if msg.startswith('[選單]'):
+    def detect(self, msg):
+        """判斷是否為選單事件
+        Params:
+            - msg
+        Return:
+            - isRichmenu(bool): 若開頭為 [選單]
+        """
+
+        if msg.startswith("[選單]"):
             return True
         else:
             return False
@@ -46,42 +47,52 @@ class RichmenuHandler:
         recnews = []
         news = []
 
-        if em == "校園公車時間表": #回覆公車路線template
+        if em == "校園公車時間表":  # 回覆公車路線template
             self.line_bot_api.reply_message(reply_token, busT.bus_route_template())
 
-        elif em == '清華校內工讀':
+        elif em == "清華校內工讀":
             recruitment_list = recruitmentUtil.get_list()
-            self.line_bot_api.reply_message(reply_token, recruitmentT.recruitment_carousel(recruitment_list))
-            
+            self.line_bot_api.reply_message(
+                reply_token, recruitmentT.recruitment_carousel(recruitment_list)
+            )
+
         # 確認機制寫在選單裡面，確保使用者不會卡死在主動推播選單。
         elif em == "切換主動推播":
             ids, err = user.getBroadcastAudienceIds(user_id)
-            self.line_bot_api.reply_message(reply_token, broadcastT.broadcast_info(len(ids) > 0)) 
+            self.line_bot_api.reply_message(
+                reply_token, broadcastT.broadcast_info(len(ids) > 0)
+            )
         elif em == "關閉主動推播":
             user.updateBroadcastTag(user_id, 2000000000)
-            self.line_bot_api.reply_message(reply_token, TextSendMessage(text="主動推播已經關閉囉，我會安靜的汪"))
+            self.line_bot_api.reply_message(
+                reply_token, TextSendMessage(text="主動推播已經關閉囉，我會安靜的汪")
+            )
         elif em == "開啟主動推播":
             user.updateBroadcastTag(user_id, 0)
-            self.line_bot_api.reply_message(reply_token, TextSendMessage(text="主動推播已經開啟囉"))
-         
+            self.line_bot_api.reply_message(
+                reply_token, TextSendMessage(text="主動推播已經開啟囉")
+            )
+
         elif em == "校務專區":
-            reply_arr=[]
+            reply_arr = []
             reply_arr.append(affairT.affair_info_carousel())
             reply_arr.append(introT.intro_carousel())
             reply_arr.append(toolsT.tools_carousel())
-            self.line_bot_api.reply_message(reply_token, reply_arr) #神奇海螺先放來這裡
-            
+            self.line_bot_api.reply_message(reply_token, reply_arr)  # 神奇海螺先放來這裡
+
         elif em == "校園地圖查詢":
             location, err = user.getMapRecord(user_id)
             if err:
                 self.line_bot_api.reply_message(reply_token, TextSendMessage(text=err))
-            elif not location:  #若取得map record為空
+            elif not location:  # 若取得map record為空
                 self.line_bot_api.reply_message(reply_token, locationT.intro())
             elif location:
                 items = locationT.create_location_list(location)
-                self.line_bot_api.reply_message(reply_token, locationT.personal_intro(items))
-            self.user.setFlag(user_id, 'mapping')
-        
+                self.line_bot_api.reply_message(
+                    reply_token, locationT.personal_intro(items)
+                )
+            self.user.setFlag(user_id, "mapping")
+
         # elif em == "新型冠狀病毒相關公告":
         #     self.line_bot_api.reply_message(reply_token, epidemicT.epidemic_info_carousel())
         # elif em == "新增新型冠狀病毒相關問題":  #ˋ4/5先拔除
@@ -90,21 +101,20 @@ class RichmenuHandler:
 
         elif em == "防疫Q&A":
             self.line_bot_api.reply_message(reply_token, epidemicT.qa_info())
-            self.user.setFlag(user_id, 'epidemic_qa')
+            self.user.setFlag(user_id, "epidemic_qa")
 
         elif em == "運勢":
-            fortune = random.choice(['超凶', '大凶', '凶', '末吉', '吉','中吉','大吉'])
+            fortune = random.choice(["超凶", "大凶", "凶", "末吉", "吉", "中吉", "大吉"])
             self.line_bot_api.reply_message(reply_token, TextSendMessage(text=fortune))
         elif em == "吃什麼":
-            eat = random.choice(['熱情小7', '風雲', '水木','小吃部','熊本'])
+            eat = random.choice(["熱情小7", "風雲", "水木", "小吃部", "熊本"])
             self.line_bot_api.reply_message(reply_token, TextSendMessage(text=eat))
 
-        #elif em == "哈哈":
-            # TODO: Fix richmenu
-            # self.line_bot_api.link_rich_menu_to_user(user_id, "richmenu-40a91b3a104119f90757b48253ab9c11")
-        #elif em == "運勢":
+        # elif em == "哈哈":
+        # TODO: Fix richmenu
+        # self.line_bot_api.link_rich_menu_to_user(user_id, "richmenu-40a91b3a104119f90757b48253ab9c11")
+        # elif em == "運勢":
         #    self.line_bot_api.reply_message(reply_token, introT.intro_carousel())
         # TODO: USR專區
         # elif em == '清華大學USR':
         #     self.line_bot_api.reply_message(reply_token, usrT.usr_carousel())
-
